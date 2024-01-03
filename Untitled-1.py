db2d24761d7dd024e295e4363ef61fea23d5be89
claude0237 <claudenkoma@gmail.com>
2024-01-03 20:20:16 +0100

    first commit

---
+++ Untitled-1.py
@@ -0,0 +1,3 @@
+age = 26
+age1 = 20
+phrase= "J'ai {} ans, {} ce n'est pas tant d'annees".format(age, age1)
---
+++ main.py
@@ -0,0 +1,4 @@
+nombre1 = int(input("Entrez un premier nombre: "))
+nombre2 = int(input("Entrez un deuxieme nombre: "))
+resultat = f"Le resultat de l'addition de {nombre1} avec {nombre2} est egale a {nombre1+nombre2}"
+print(resultat)
